a597d660e3d28cdb52b1e7ea0752e88b71f84729
Abdulaziz AX <abdulazizax2001@gmail.com>
2024-04-26 00:38:09 +0500

    second

--- git/git/git.go
+++ git/git/git.go
@@ -12,3 +12,12 @@ func GetUserName() (string, error) {
 	}
 	return string(out), nil
 }
+
+func GetUserEmail() (string, error) {
+	cmd := exec.Command("git", "config", "--get", "user.email")
+	out, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
+}
