6a99416fa858975d35246430483d1b72910a58cf
Abdulaziz AX <abdulazizax2001@gmail.com>
2024-04-26 00:26:56 +0500

    first

---
+++ git/git/git.go
@@ -0,0 +1,14 @@
+package git
+
+import (
+	"os/exec"
+)
+
+func GetUserName() (string, error) {
+	cmd := exec.Command("git", "config", "--get", "user.name")
+	out, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
+}
---
+++ git/go.mod
@@ -0,0 +1,3 @@
+module github.com/abdulazizax/git
+
+go 1.22.2
